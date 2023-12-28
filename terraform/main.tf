43d962cd658fe5b94d2ce33706c86abb9290652a
lharantes <79605326+lharantes@users.noreply.github.com>
2023-12-28 14:04:57 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "RG-TERRAFORM-STATE"
     storage_account_name = "stotfstatearantes"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
