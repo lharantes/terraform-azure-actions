80f672e732ace0c6e709db3d6c78438ae26addf1
lharantes <79605326+lharantes@users.noreply.github.com>
2023-12-28 13:43:46 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "random_pet" "rg-name" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name      = random_pet.rg-name.id
+  name      ="RG-teste"
   location  = var.resource_group_location
 }
 
