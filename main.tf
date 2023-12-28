63c4ed0828e09480158b88077a410d9d04f11ced
lharantes <79605326+lharantes@users.noreply.github.com>
2023-12-28 16:23:37 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Create subnet
+# Create subnet a
 resource "azurerm_subnet" "myterraformsubnet" {
   name                 = "mySubnet"
   resource_group_name  = azurerm_resource_group.rg.name
